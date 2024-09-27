51d7c5ad4d5798a5d5b8f9427856c2806740bd86
evgeniy-kulikov <redstepway@gmail.com>
2024-09-27 21:58:31 +0300

    added elements to activity_main & start of MainActivity logic

--- app/src/main/java/ru/eugenypets/itprogerapp/MainActivity.kt
+++ app/src/main/java/ru/eugenypets/itprogerapp/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.eugenypets.itprogerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Находим элементы
+        val userLogin: EditText = findViewById(R.id.etxtLogin)
+        val userEmail: EditText = findViewById(R.id.etxtEmail)
+        val userPassword: EditText = findViewById(R.id.etxtPassword)
+        val btnSubmit: Button = findViewById(R.id.btnRegistration)
+
+        // слушатели событий элементов
+        btnSubmit.setOnClickListener {
+            // собираем данные полей ввода
+            val login = userLogin.text.toString().trim()
+            val email = userEmail.text.toString().trim()
+            val pass = userPassword.text.toString().trim()
+
+            if (login == "" || email == "" || pass == "")
+                // всплывающее уведомление
+                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            else {
+                println(pass)
+            }
+
+        }
+
     }
 }
